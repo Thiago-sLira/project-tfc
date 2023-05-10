9d079ac1e58ad1dbbb82d449b5fae4fdfccf4f7e
Thiago Lira <thiago.slira@outlook.com>
2023-05-10 16:26:31 -0300

    Criando model de Teams

--- app/backend/src/models/TeamModel.ts
+++ app/backend/src/models/TeamModel.ts
@@ -1,3 +1,34 @@
-// import Team from '../database/models/Team';
+import { Team } from '../database/models';
 
-// export default class
+export default class TeamModel {
+  private _teams = Team;
+
+  constructor(teams: Team) {
+    this._teams = teams;
+  }
+
+  // async createTeam(teamName: string) {
+  //   const team = await this.create({ teamName });
+  //   return team;
+  // }
+
+  // async getAllTeams() {
+  //   const teams = await this.findAll();
+  //   return teams;
+  // }
+
+  // async getTeamById(id: number) {
+  //   const team = await this.findByPk(id);
+  //   return team;
+  // }
+
+  // async updateTeam(id: number, teamName: string) {
+  //   const team = await this.update({ teamName }, { where: { id } });
+  //   return team;
+  // }
+
+  // async deleteTeam(id: number) {
+  //   const team = await this.destroy({ where: { id } });
+  //   return team;
+  // }
+}
