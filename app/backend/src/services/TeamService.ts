331ea30700110b081bd0cef4756725ffefa76157
Thiago Lira <thiago.slira@outlook.com>
2023-05-10 17:00:42 -0300

    Renomeado atributo teamModel de service

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,10 +1,10 @@
 import TeamModel from '../models/TeamModel';
 
 export default class TeamService {
-  constructor(private _teamModel = new TeamModel()) { }
+  constructor(private teamModel = new TeamModel()) { }
 
   async getAllTeams() {
-    const allTeams = await this._teamModel.getAllTeams();
+    const allTeams = await this.teamModel.getAllTeams();
     return allTeams;
   }
 }
