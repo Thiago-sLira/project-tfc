9f7df0b89f3b969fdc5a1ae5ac214309765ba482
Thiago Lira <thiago.slira@outlook.com>
2023-05-11 18:21:05 -0300

    Criada rota de /login com método POST

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import teamRoutes from './team.routes';
+import userRoutes from './user.routes';
 
 const router = Router();
 
 router.use('/teams', teamRoutes);
+router.use('/users', userRoutes);
 
 export default router;
