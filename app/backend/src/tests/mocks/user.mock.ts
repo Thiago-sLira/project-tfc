e24b67c7b36d8f07af37062d5b718f29cd90c913
Thiago Lira <thiago.slira@outlook.com>
2023-05-11 17:43:32 -0300

    Teste TDD para validar um login com sucesso

---
+++ app/backend/src/tests/mocks/user.mock.ts
@@ -0,0 +1,11 @@
+const mockUserData = {
+  id: 1,
+  username: 'Testes dos Testes',
+  role: 'Teste',
+  password: '123456',
+  email: 'teste@teste.com',
+};
+
+export {
+  mockUserData,
+};
--- app/backend/src/tests/user.test.ts
+++ app/backend/src/tests/user.test.ts
@@ -2,7 +2,7 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
-import * as mock from './mocks/user.mock';
+import { mockUserData } from './mocks/user.mock';
 import User from '../database/models/User';
 
 import { app } from '../app';
@@ -26,46 +26,50 @@ describe('Testes do endpoint /login', () => {
   // Verifica se não é possível fazer um login com senha errada
 
   describe('Testes do método POST de /login', () => {
-    beforeEach(() => {
-      modelUserStub = sinon.stub(Team, 'findAll')
-        .resolves(mockAllTeams as unknown as Team[]);
-    });
-
     it('Verifica se possível fazer um login com sucesso', async () => {
-      const response = await chai.request(app).get('/teams');
+      before(() => {
+        modelUserStub = sinon.stub(User, 'findOne')
+          .resolves(mockUserData as unknown as User);
+      });
+      const response = await chai.request(app)
+        .post('/login')
+        .send({
+          email: 'teste@teste.com',
+          password: '123456',
+        });
 
       expect(response.status).to.be.equal(200);
-      expect(response.body).to.be.deep.equal(mockAllTeams);
+      expect(response.body.token).not.to.be.empty;
     });
-    it('Verifica se não é possível fazer um login sem email', async () => {
-      const response = await chai.request(app).get('/teams');
+    // it('Verifica se não é possível fazer um login sem email', async () => {
+    //   const response = await chai.request(app).get('/teams');
 
-      expect(response.status).to.be.equal(200);
-      expect(response.body).to.be.deep.equal(mockAllTeams);
-    });
-    it('Verifica se não é possível fazer um login com email invalido', async () => {
-      const response = await chai.request(app).get('/teams');
+    //   expect(response.status).to.be.equal(200);
+    //   expect(response.body).to.be.deep.equal(mockAllTeams);
+    // });
+    // it('Verifica se não é possível fazer um login com email invalido', async () => {
+    //   const response = await chai.request(app).get('/teams');
 
-      expect(response.status).to.be.equal(200);
-      expect(response.body).to.be.deep.equal(mockAllTeams);
-    });
-    it('Verifica se não é possível fazer um login com email não cadastrado', async () => {
-      const response = await chai.request(app).get('/teams');
+    //   expect(response.status).to.be.equal(200);
+    //   expect(response.body).to.be.deep.equal(mockAllTeams);
+    // });
+    // it('Verifica se não é possível fazer um login com email não cadastrado', async () => {
+    //   const response = await chai.request(app).get('/teams');
 
-      expect(response.status).to.be.equal(200);
-      expect(response.body).to.be.deep.equal(mockAllTeams);
-    });
-    it('Verifica se não é possível fazer um login sem senha', async () => {
-      const response = await chai.request(app).get('/teams');
+    //   expect(response.status).to.be.equal(200);
+    //   expect(response.body).to.be.deep.equal(mockAllTeams);
+    // });
+    // it('Verifica se não é possível fazer um login sem senha', async () => {
+    //   const response = await chai.request(app).get('/teams');
 
-      expect(response.status).to.be.equal(200);
-      expect(response.body).to.be.deep.equal(mockAllTeams);
-    });
-    it('Verifica se não é possível fazer um login com senha errada', async () => {
-      const response = await chai.request(app).get('/teams');
+    //   expect(response.status).to.be.equal(200);
+    //   expect(response.body).to.be.deep.equal(mockAllTeams);
+    // });
+    // it('Verifica se não é possível fazer um login com senha errada', async () => {
+    //   const response = await chai.request(app).get('/teams');
 
-      expect(response.status).to.be.equal(200);
-      expect(response.body).to.be.deep.equal(mockAllTeams);
-    });
+    //   expect(response.status).to.be.equal(200);
+    //   expect(response.body).to.be.deep.equal(mockAllTeams);
+    // });
   });
 });
