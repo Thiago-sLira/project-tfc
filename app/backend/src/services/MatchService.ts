94991a9e5d766a1c51ee2db5b320183a5613098f
Thiago Lira <thiago.slira@outlook.com>
2023-05-12 19:16:44 -0300

    Implementado no método createMatch função para buscar o homeTeam no banco de dados

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -28,6 +28,8 @@ export default class MatchService {
   }
 
   async createMatch(match: CreateMatch) {
+    const homeTeam = await this.teamModel.getTeamById(match.homeTeamId);
+
     const matchCreated = await this.matchModel.createMatch(match);
     return matchCreated;
   }
