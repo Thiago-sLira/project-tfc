540d64e35b835cf014d570b1edbf2b860a905012
Thiago Lira <thiago.slira@outlook.com>
2023-05-12 19:12:19 -0300

    Adicionado middleware para validar times iguais no routes

--- app/backend/src/routes/match.routes.ts
+++ app/backend/src/routes/match.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import MatchController from '../controllers/MatchController';
 import validateToken from '../middlewares/validateToken';
+import validateNewMatchParams from '../middlewares/validateNewMatchParams';
 
 const router = Router();
 
@@ -8,7 +9,12 @@ const matchController = new MatchController();
 router.get('/', (req, res) => matchController.getAllMatches(req, res));
 router.patch('/:id/finish', validateToken, (req, res) => matchController.finishMatch(req, res));
 router.patch('/:id', validateToken, (req, res) => matchController.updateMatch(req, res));
-router.post('/', validateToken, (req, res) => matchController.createMatch(req, res));
+router.post(
+  '/',
+  validateToken,
+  validateNewMatchParams,
+  (req, res) => matchController.createMatch(req, res),
+);
 // router.get('/:id', (req, res) => matchController.getTeamById(req, res));
 
 export default router;
