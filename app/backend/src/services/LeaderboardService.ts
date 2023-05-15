3f384e85dcb889b95e426127ff33977f296aecde
Thiago Lira <thiago.slira@outlook.com>
2023-05-15 18:30:54 -0300

    Criado método para buscar o desempenho de todos os times

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -5,10 +5,19 @@ export default class LeaderboardService {
   constructor(
     private matchModel = new MatchModel(),
     private teamModel = new TeamModel(),
+    
   ) { }
 
+  
+
+  
+
   async getAllHomeTeamsPerformance() {
-    const allMatches = await this.matchModel.getAllMatchesInProgress(false);
-    return 'oi';
+    const allMatches = await this.matchModel.getAllMatchesInProgressWithoutScope(false);
+    const allTeams = await this.teamModel.getAllTeams();
+
+    const allHomeTeamsPerformance = this.allHomeTeamsPerformance(allMatches, allTeams);
+
+    return allHomeTeamsPerformance;
   }
 }
