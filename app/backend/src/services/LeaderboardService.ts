276cf8f6dec39afd9ac1c3fa08b463f71bc4dba6
Thiago Lira <thiago.slira@outlook.com>
2023-05-15 16:44:27 -0300

    Implementado método para chamar a model

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -5,5 +5,8 @@ import LeaderboardModel from '../models/LeaderboardModel';
 export default class LeaderboardService {
   constructor(private leaderboardModel = new LeaderboardModel()) { }
 
-  
+  async getAllHomeTeamsPerformance() {
+    const allHomeTeamsPerformance = await this.leaderboardModel.getAllHomeTeamsPerformance();
+    return allHomeTeamsPerformance;
+  }
 }
