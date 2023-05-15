b671e159c8dd50d2ac264c2739b57775d606edd5
Thiago Lira <thiago.slira@outlook.com>
2023-05-15 17:09:24 -0300

    Atualizado leaderboard.routes para receber a rota /leaderboard/home

--- app/backend/src/routes/leaderboard.routes.ts
+++ app/backend/src/routes/leaderboard.routes.ts
@@ -4,6 +4,6 @@ import LeaderboardController from '../controllers/LeaderboardController';
 const router = Router();
 
 const leaderboardController = new LeaderboardController();
-router.get('/', (req, res) => leaderboardController.getAllHomeTeamsPerformance(req, res));
+router.get('/home', (req, res) => leaderboardController.getAllHomeTeamsPerformance(req, res));
 
 export default router;
