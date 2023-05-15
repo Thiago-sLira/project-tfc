5e9f207bbe299afeea1ac3f42018ee43951359cb
Thiago Lira <thiago.slira@outlook.com>
2023-05-15 16:39:02 -0300

    Alterado nome da função sendo chamada no routes para ficar mais descritiva

--- app/backend/src/routes/leaderboard.routes.ts
+++ app/backend/src/routes/leaderboard.routes.ts
@@ -4,7 +4,7 @@ import LeaderboardController from '../controllers/LeaderboardController';
 const router = Router();
 
 const leaderboardController = new LeaderboardController();
-router.get('/', (req, res) => leaderboardController.getAllTeamsPerformance(req, res));
+router.get('/', (req, res) => leaderboardController.getAllHomeTeamsPerformance(req, res));
 // router.get('/:id', (req, res) => leaderboardController.getTeamById(req, res));
 
 export default router;
