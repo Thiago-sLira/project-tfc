e077ca7461cbeb57f7644af4b3ad226f2d5317b0
Thiago Lira <thiago.slira@outlook.com>
2023-05-15 16:34:45 -0300

    Criado arquivo de routes para a rota /leaderboard

---
+++ app/backend/src/routes/leaderboard.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import LeaderboardController from '../controllers/LeaderboardController';
+
+const router = Router();
+
+const leaderboardController = new LeaderboardController();
+router.get('/', (req, res) => leaderboardController.getAllTeamsPerformance(req, res));
+// router.get('/:id', (req, res) => leaderboardController.getTeamById(req, res));
+
+export default router;
