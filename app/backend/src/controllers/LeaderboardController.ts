c4a39f04f121f55e2d962e51f9e6a72da5c98cb8
Thiago Lira <thiago.slira@outlook.com>
2023-05-15 16:40:04 -0300

    Adicionado constructor na classe LeaderboardController

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -2,5 +2,7 @@ import { Request, Response } from 'express';
 import LeaderboardService from '../services/LeaderboardService';
 
 export default class LeaderboardController {
+  constructor(private leaderboardService = new LeaderboardService()) { }
+
   
 }
