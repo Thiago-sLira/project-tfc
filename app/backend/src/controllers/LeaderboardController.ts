b30c163c4c7e3c6af2a3e5a1e311a569de141e5b
Thiago Lira <thiago.slira@outlook.com>
2023-05-15 16:40:34 -0300

    Método responsável por ser chamado na rota criado

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -4,5 +4,8 @@ import LeaderboardService from '../services/LeaderboardService';
 export default class LeaderboardController {
   constructor(private leaderboardService = new LeaderboardService()) { }
 
-  
+  async getAllHomeTeamsPerformance(_req: Request, res: Response) {
+    const allHomeTeamsPerformance = await this.leaderboardService.getAllHomeTeamsPerformance();
+    res.status(200).json(allHomeTeamsPerformance);
+  }
 }
