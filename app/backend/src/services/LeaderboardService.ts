303f1b120444808681c2301839d34e38410fb163
Thiago Lira <thiago.slira@outlook.com>
2023-05-15 17:06:28 -0300

    Implementado consteructor para receber TeamModel

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -1,7 +1,11 @@
+import TeamModel from '../models/TeamModel';
 import MatchModel from '../models/MatchModel';
 
 export default class LeaderboardService {
-  constructor(private matchModel = new MatchModel()) { }
+  constructor(
+    private matchModel = new MatchModel(),
+    private teamModel = new TeamModel(),
+  ) { }
 
   async getAllHomeTeamsPerformance() {
     const allMatches = await this.matchModel.getAllMatchesInProgress(false);
