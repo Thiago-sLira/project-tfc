d7bcf2855b877199ea0695e349186d4ae934868c
Thiago Lira <thiago.slira@outlook.com>
2023-05-15 21:06:40 -0300

    Criada rota para buscar o desempenho dos times

--- app/backend/src/routes/leaderboard.routes.ts
+++ app/backend/src/routes/leaderboard.routes.ts
@@ -5,5 +5,6 @@ const router = Router();
 
 const leaderboardController = new LeaderboardController();
 router.get('/home', (req, res) => leaderboardController.getAllHomeTeamsPerformance(req, res));
+router.get('/away', (req, res) => leaderboardController.getAllAwayTeamsPerformance(req, res));
 
 export default router;
